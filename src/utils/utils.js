b9981de0a09ed6830880908ffa267b7ac8132318
Zephyr <Zephyrtmh@gmail.com>
2022-05-06 02:24:01 +0800

    added sort by distance function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -50,11 +50,56 @@ export async function getPlaces(keyword, lat, lng) {
     
 }
 
-export function filterShops(filterBy, shopsList) {
-    shopsList.sort((first, second) => {
-        return second[filterBy] - first[filterBy]
-    })
-    console.log("new list")
-    console.log(shopsList)
+export function filterShops(filterBy="default", shopsList, currLoc) {
+    switch(filterBy) {
+        case "default":
+            console.log("filter shops by default")
+            break;
+
+        case "rating":
+            console.log("filter shops by ratings")
+            shopsList.sort((first, second) => {
+                return second["rating"] - first["rating"]
+            })
+            console.log("new list")
+            console.log(shopsList)
+            break;
+
+        case "distance":
+            console.log("filter shops by distance")
+            console.log(currLoc)
+            shopsList.sort((first, second) => {
+                console.log(second['geometry']['location'], first['geometry']['location'])
+                let dist2 = calcDist(currLoc, second['geometry']['location']);
+                let dist1 = calcDist(currLoc, first['geometry']['location']);
+                console.log(dist2, dist1)
+                return dist1 - dist2
+            })
+            console.log("new list")
+            console.log(shopsList)
+            break;
+    } 
+    
     return shopsList
 }
+
+
+
+export function calcDist(curr, dest) {
+    let currLat = curr.lat;
+    let currLng = curr.lng;
+    let destLat = dest.lat;
+    let destLng = dest.lng;
+    //convert to Radians
+    currLat = currLat/(180/Math.PI);
+    destLat = destLat/(180/Math.PI);
+    currLng = currLng/(180/Math.PI);
+    destLng = destLng/(180/Math.PI);
+
+    const diffLat = destLat - currLat;
+    const diffLng = destLng - currLng;
+    //radius of Earth in KM = 6371
+    //Very loooooong formula to calculate distance that I definitely did not google for
+    const distance = 6371 * 2* Math.asin(Math.sqrt(Math.pow(Math.sin(diffLat/2),2) + Math.cos(currLat) * Math.cos(destLat) * Math.pow(Math.sin(diffLng / 2), 2)));
+    return distance
+}   
